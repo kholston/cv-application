f99be8d1f263d05beea10aefebd3d0599a8d5c2f
Ken Holston <git.kholston@gmail.com>
2020-11-13 12:27:58 -0500

    setup state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>CV App</h1>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import Education from './components/Education';
+import General from './components/General';
+import Practical from './components/Practical';
+
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name:"",
+      email:"",
+      phone:"",
+      school_name:"",
+      study_title:"",
+      study_date:"",
+      company_name:"",
+      position_title:"",
+      main_tasks:"",
+      date_start:"",
+      date_end:"",
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({
+      ...this.state,
+      [e.target.name]: value,
+    })
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+  }
+
+  render(){
+    return (
+      <div className="col-6 mx-auto mt-5">
+        <header className="App-header">
+          <h1 className="text-center">CV Application</h1>
+        </header>
+        <General onChange={this.handleChange} name={this.state.name} />
+      </div>
+    )
+  }
+  
 }
 
 export default App;
