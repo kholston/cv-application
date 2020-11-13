1ad199a6893052fc832a7e56038868af61fd833d
Ken Holston(Work) <git.kholston@gmail.com>
2020-11-13 23:23:28 +0000

    add edit state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Education from './components/Education';
 import General from './components/General';
 import Practical from './components/Practical';
 
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      name:"",
-      email:"",
-      phone:"",
-      school_name:"",
-      study_title:"",
-      study_date:"",
-      company_name:"",
-      position_title:"",
-      main_tasks:"",
-      date_start:"",
-      date_end:"",
-
+      name: "",
+      email: "",
+      phone: "",
+      school_name: "",
+      study_title: "",
+      study_date: "",
+      company_name: "",
+      position_title: "",
+      main_tasks: "",
+      date_start: "",
+      date_end: "",
+      general_edit: true,
+      education_edit: true,
+      practical_edit: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeEditState = this.changeEditState.bind(this);
   }
 
   handleChange = (e) => {
@@ -35,22 +38,31 @@ class App extends Component {
     })
   }
 
-  handleSubmit = (e) =>{
+  handleSubmit = (e) => {
     e.preventDefault();
+    alert(e.target.value)
   }
 
-  render(){
+  changeEditState = (name) => {
+    const value = this.state[name];
+    this.setState({
+      ...this.state,
+      [name]: !value,
+    })
+  }
+
+  render() {
     return (
       <div className="col-6 mx-auto mt-5">
         <header className="App-header">
           <h1 className="text-center">CV Application</h1>
         </header>
-        <General onChange={this.handleChange} currentState={this.state} />
+        <General onChange={this.handleChange} currentState={this.state} edit={this.changeEditState}/>
         <hr/>
       </div>
     )
   }
-  
+
 }
 
 export default App;
